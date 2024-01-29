ac544c2543a26117e3d1e0804f1ec49ad7845be8
mdimran24 <mohammedimran0724@gmail.com>
2024-01-29 12:19:49 +0000

    JWT implementation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema
+const validator = require('validator')
 
 const userSchema = new Schema({
     email: {
@@ -16,7 +17,18 @@ const userSchema = new Schema({
 
 // static sign up method
 userSchema.statics.signup = async function(email, password) {
-
+    
+    // validation
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)){
+        throw Error('Email is not valid')
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error('Password not strong enough')
+    }
+    
     const exists = await this.findOne({ email })
 
     if(exists){
