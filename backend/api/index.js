bd939ea6f5d8ecb9ba96986950d3bd907cb0ab61
mdimran24 <mohammedimran0724@gmail.com>
2024-03-31 19:29:08 +0100

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,48 +1,49 @@
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 
 // express app
-const app = express()
-const mongoose = require('mongoose')
-const appointmentRoutes = require('../routes/appointments')
-const pricesRoutes = require("../routes/prices")
-const userRoutes = require('../routes/user')
-
+const app = express();
+const mongoose = require("mongoose");
+const appointmentRoutes = require("../routes/appointments");
+const pricesRoutes = require("../routes/prices");
+const userRoutes = require("../routes/user");
 
 //middleware
-app.use(express.json())
-app.use(cors(
-));
+app.use(express.json());
+const corsOpts = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET','POST','HEAD','PUT','PATCH','DELETE'],
+    allowedHeaders: ['Content-Type'],
+    exposedHeaders: ['Content-Type']
+};
+app.use(cors(corsOpts));
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
-
+  console.log(req.path, req.method);
+  next();
+});
 
 //routes
-app.use('/api/appointments', appointmentRoutes)
-app.use('/api/user', userRoutes)
-app.use('/api/prices', pricesRoutes)
-
-
+app.use("/api/appointments", appointmentRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/prices", pricesRoutes);
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     // listen for requests
-if(process.env.PORT){
-app.listen(process.env.PORT, () =>{
-    console.log('Listening on Port ' + process.env.PORT)
-})
-}
+    if (process.env.PORT) {
+      app.listen(process.env.PORT, () => {
+        console.log("Listening on Port " + process.env.PORT);
+      });
+    }
+  })
 
+  .catch((error) => {
+    console.log(error);
+  });
 
-})
-
-.catch((error) => {
-    console.log(error)
-})
-
-module.exports = app
+module.exports = app;
